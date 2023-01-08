f307aa6baf53e1b1080b889d4a6df33cca8f5658
Nishant <nishantsoftwarelearning1@gmail.com>
2023-01-08 18:26:34 +0530

    This is arraylist commited

---
+++ src/com/velocity/arraylist/Arraylist.java
@@ -0,0 +1,20 @@
+package com.velocity.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		
+		List<String> l=new ArrayList<String>();
+		
+		l.add("nishant");
+		l.add("bharat");
+		l.add("jalkotkar");
+		
+		//System.out.println(l);
+		for(String list:l) {
+			System.out.println(list);
+		}
+	}}
